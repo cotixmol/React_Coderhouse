311416744e071116f5eb70f9beed9ea5f25195cb
cotixmol <constancio.molinengo@outlook.com>
2022-09-16 00:36:44 -0300

    Listo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+//Importo las funciones de React.
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//Importo la hoja de estilos de app.
 import './App.css';
-
+//Importo los componentes que son hijos de App()
 import NavBarPersonalizada from './components/NavBar/NavBarPersonalizada.js'
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
-
-import {CartProvider} from "./context/cartContext"
 import { CartContainer } from './components/CartContainer/CartContainer';
+//Importo los componentes del contexto.
+import { CartProvider } from "./context/cartContext"
+
 
 
 function App() {
   return (
+
     <BrowserRouter>
 
       <CartProvider>
@@ -29,6 +33,7 @@ function App() {
       </CartProvider>
 
     </BrowserRouter>
+    
   );
 }
 
